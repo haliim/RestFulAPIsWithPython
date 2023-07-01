d1055ba457363c58f9d7e90ffca12d9816fbf58f
Ahmed Halim <ah.hesham93@gmail.com>
2023-07-01 20:47:20 +0300

    creating new branch dev for development

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, from Flask'
+@app.route("/")
+def home():
+    return "Hello, World!"
 
 if __name__ == "__main__":
     app.run(debug=True)
